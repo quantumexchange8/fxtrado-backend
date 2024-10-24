4ed315868a54505ec2d612694e5049ca5fcfb30a
darren <darren.kooyz@gmail.com>
2024-10-24 15:13:30 +0800

    fix: change history_chart sec to 00

--- src/cronJobs.js
+++ src/cronJobs.js
@@ -81,7 +81,5 @@ const formatDate = (date) => {
     String(date.getUTCMonth() + 1).padStart(2, '0') + '-' +
     String(date.getUTCDate()).padStart(2, '0') + ' ' +
     String(date.getUTCHours()).padStart(2, '0') + ':' +
-    String(date.getUTCMinutes()).padStart(2, '0') + ':' +
-    String(date.getUTCSeconds()).padStart(2, '0') + '.' +
-    String(date.getUTCMilliseconds()).padStart(3, '0');
+    String(date.getUTCMinutes()).padStart(2, '0') + '.00';
 };
